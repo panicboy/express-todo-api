178b22963cca2f17dd9538be16bdb9d0bb79f6db
panicboy <asasaki@gmail.com>
2016-07-13 18:38:41 -1000

    updated validation

--- routes/buzzword.js
+++ routes/buzzword.js
@@ -114,6 +114,8 @@ function validateParams(body, paramObject, paramVals){
   for (var i = 0; i <= testParams.length -1; i++) {
     let param = testParams[i];
     if(!body.hasOwnProperty(param)) return false;
+    if(paramObject[param] == 'boolean' && boolTest(theVal)) body[param] = boolify(body[param]);
+    if(paramObject[param] == 'number' && numTest(body[param])) body[param] = Number(body[param]);
     // console.log(param,': ', body[param], ', type: ', typeof body[param], ', expected type: ', paramObject[param]);
     if(typeof body[param] != paramObject[param]) return false;
     if((checkParamVals && paramVals.hasOwnProperty(param)) &&  body[param] != paramVals[param]) return false;
@@ -131,11 +133,23 @@ return theData;
 }
 
 function coerceBoolean(theVal) {
-  if(['true',true,'false', false].indexOf(theVal) >= 0) return (theVal == 'true' || theVal === true);
+  if(boolTest(theVal)) return boolify(theVal);
   return null;
 }
 
 function coerceNumber(theVal) {
-  if(Number(theVal)==theVal) return Number(theVal);
+  if(numTest(theVal)) return Number(theVal);
   return null;
+}
+
+function boolTest(theVal) {
+  return (['true',true,'false', false].indexOf(theVal) >= 0);
+}
+
+function numTest(theVal) {
+  return (Number(theVal) == theVal);
+}
+
+function boolify(theVal) {
+  return (theVal == 'true' || theVal === true);
 }
